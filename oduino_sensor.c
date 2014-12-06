45400eaf042bcc7b8f438d24137761ca4ad6e8b7
pdaejun <pdaejun@gmail.com>
2014-12-05 23:46:34 -0800

    oduino_senosor service complete

--- oduino_sensor.c
+++ oduino_sensor.c
@@ -1,37 +1,34 @@
 #include <linux/module.h>
-
+#include <linux/string.h>
 #include <linux/init.h>
-#include <linux/fs.h>
-#include <linux/platform_device.h>
-#include <linux/input.h>
+#include <linux/sysfs.h>
+#include <linux/device.h>
 
-static int temp=0, hum=0;
-static struct platform_device *oduinoSensor;
+static int temp;
+static int hum;
+struct class *VsensorClass;
+struct device *odev;
 
 static ssize_t sensorShow(struct device *dev, struct device_attribute *attr, 
 				char *buffer){
-	sprintf(buffer, "%d %d", temp, hum);
-	return 0;
+	return sprintf(buffer, "%d %d\n", temp, hum);
 }
 
 static ssize_t sensorStore(struct device *dev, struct device_attribute *attr, 
 				const char *buffer, size_t count){
 	sscanf(buffer, "%d %d", &temp, &hum);
-	return 0;
+	return count;
 }
 
-DEVICE_ATTR(oduino_sensor, 0644, sensorShow, sensorStore);
-
+DEVICE_ATTR(oduino_sensor, 0777, sensorShow, sensorStore);
 
 
 static int __init oduinoSensor_init(void){
-	
-	oduinoSensor = platform_device_register_simple("oduinoSensor", -1, NULL, 0);
-	if(IS_ERR(oduinoSensor)){
-		printk("oduinoSensor_init : error during platfrom device register\n");
-	}
-
- 	sysfs_create_file(&oduinoSensor->dev.kobj, &dev_attr_oduino_sensor.attr);	
+	VsensorClass = class_create(THIS_MODULE, "Vsensor");
+	odev = device_create(VsensorClass, NULL, 0, NULL, "oduino");
+	//oduinoSensor = platform_device_register_simple("oduinoSensor", -1, NULL, 0);
+	device_create_file(odev,  &dev_attr_oduino_sensor);
+ //	sysfs_create_file(&oduinoSensor->dev.kobj, &dev_attr_oduino_sensor.attr);	
 
 	return 0;
 	
@@ -39,9 +36,11 @@ static int __init oduinoSensor_init(void){
 
 static void __exit oduinoSensor_exit (void){
 
-	sysfs_remove_file(&oduinoSensor->dev.kobj, &dev_attr_oduino_sensor.attr);
-	platform_device_unregister(oduinoSensor);
-
+//	sysfs_remove_file(&oduinoSensor->dev.kobj, &dev_attr_oduino_sensor.attr);
+//	platform_device_unregister(oduinoSensor);
+	device_destroy(VsensorClass, 0);
+	class_destroy(VsensorClass);
+	device_remove_file(odev, &dev_attr_oduino_sensor);
 	return;
 }
 
@@ -49,3 +48,4 @@ module_init(oduinoSensor_init);
 module_exit(oduinoSensor_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("virtual input device");
